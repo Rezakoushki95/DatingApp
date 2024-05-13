744ba67d22a47984cdee94f5dc0dcad725a13e30
Rezakoushki95 <41579107+Rezakoushki95@users.noreply.github.com>
2024-05-13 03:01:44 +0200

    Add validation when registering user

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -3,33 +3,33 @@ using API.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace API.Controllers;
-
-[ApiController]
-[Route("api/[controller]")]
-public class UsersController : ControllerBase
+namespace API.Controllers
 {
-    private readonly DataContext _context;
-
-    public UsersController(DataContext context)
+    public class UsersController : BaseApiController
     {
-        _context = context;
-    }
+        private readonly DataContext _context;
 
-    [HttpGet]
-    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
-    {
-        var users = await _context.Users.ToListAsync();
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
 
-        return users;
-    }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var users = await _context.Users.ToListAsync();
 
-    [HttpGet("{id}")] // /api/users/2
-    public async Task<ActionResult<AppUser>> GetUser(int id)
-    {
-        var user = await _context.Users.FindAsync(id);
+            return users;
+        }
 
-        return user;
+        [HttpGet("{id}")] // /api/users/2
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+
+            return user;
+        }
+
     }
 
 }
