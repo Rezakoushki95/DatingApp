49662035636c8e7551000b22f9be5511e6b7b6c8
Rezakoushki95 <41579107+Rezakoushki95@users.noreply.github.com>
2024-05-14 03:20:19 +0200

    Add extension methods

---
+++ API/Extentions/ApplicationServiceExtentions.cs
@@ -0,0 +1,21 @@
+﻿using API.Data;
+using API.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Extentions;
+
+public static class ApplicationServiceExtentions
+{
+    public static IServiceCollection AddApplicationService(this IServiceCollection services, IConfiguration config)
+    {
+        services.AddDbContext<DataContext>(opt =>
+        {
+            opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
+        });
+
+        services.AddCors();
+        services.AddScoped<ITokenService, TokenService>();
+
+        return services;
+    }
+}
