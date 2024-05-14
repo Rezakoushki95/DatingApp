4abaf3b98b423842f6b38670be3cba9e10f93b08
Rezakoushki95 <41579107+Rezakoushki95@users.noreply.github.com>
2024-05-14 02:43:36 +0200

    create UserDto and return token

--- API/DTOs/UserDto.cs
+++ API/DTOs/UserDto.cs
@@ -1,8 +1,7 @@
 ﻿namespace API;
 
-public class LoginDto
+public class UserDto
 {
     public string Username { get; set; }
-    public string Password { get; set; }
-
+    public string Token { get; set; }
 }
