42fc9a238885121363d0adcdcd055b4c54dc1c81
Rezakoushki95 <41579107+Rezakoushki95@users.noreply.github.com>
2024-05-16 02:33:36 +0200

    hook up register method to account service

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, map } from 'rxjs';
-import { User } from '../_models/User';
+import { User } from '../_models/user';
 
 @Injectable({
   providedIn: 'root'
@@ -18,6 +18,17 @@ export class AccountService {
       map((response: User) => {
         const user = response;
 
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUserSource.next(user);
+        }
+      })
+    )
+  }
+
+  register(model: any) {
+    return this.http.post<User>(this.baseUrl + "account/register", model).pipe(
+      map(user => {
         if (user) {
           localStorage.setItem('user', JSON.stringify(user));
           this.currentUserSource.next(user);
