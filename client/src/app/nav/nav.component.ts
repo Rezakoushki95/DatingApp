a0f865caef030842f3de777a6061b3d5aee2617a
Rezakoushki95 <41579107+Rezakoushki95@users.noreply.github.com>
2024-05-17 01:37:23 +0200

    add toastr to login and register

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
@@ -12,7 +13,9 @@ export class NavComponent implements OnInit {
 
   constructor(
     public accountService: AccountService,
-    private router: Router) { }
+    private router: Router,
+    private toastr: ToastrService
+  ) { }
 
   ngOnInit(): void {
   }
@@ -21,7 +24,7 @@ export class NavComponent implements OnInit {
   login() {
     this.accountService.login(this.model).subscribe({
       next: () => this.router.navigateByUrl("/members"), 
-      error: error => console.log(error)
+      error: error => this.toastr.error(error.error)
     })
   }
 
