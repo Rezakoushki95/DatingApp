b52e777f349ecc37da8e1e2102f57344e098949e
Rezakoushki95 <41579107+Rezakoushki95@users.noreply.github.com>
2024-05-22 01:31:11 +0200

    create extendeduseridentiy migration representing a one to many relationship between appusers and photos

--- API/Entities/Photo.cs
+++ API/Entities/Photo.cs
@@ -1,12 +1,17 @@
-﻿using System.Security.Cryptography.X509Certificates;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Security.Cryptography.X509Certificates;
 
 namespace API.Entities;
 
+[Table("Photos")]
 public class Photo
 {
     public int Id { get; set; }
     public string Url { get; set; }
     public bool isMain { get; set; }
     public string PublicId { get; set; }
+
+    public int AppUserId { get; set; }
+    public AppUser AppUser { get; set; }
 
 }
