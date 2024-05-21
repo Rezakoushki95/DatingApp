e0b9d95f254fcd8099d5f0e1b21faf77d9314263
Rezakoushki95 <41579107+Rezakoushki95@users.noreply.github.com>
2024-05-22 00:59:44 +0200

    Extend DateOnly type with an CalculateAge method

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,4 +1,5 @@
 ﻿using System.Security.Cryptography.X509Certificates;
+using API.Extentions;
 
 namespace API.Entities;
 
@@ -19,5 +20,10 @@ public class AppUser
     public string City { get; set; }
     public string Country { get; set; }
     public List<Photo> Photos { get; set; } = new List<Photo>();
+
+    public int GetAge()
+    {
+        return DateOfBirth.CalculateAge();
+    }
 }
 
